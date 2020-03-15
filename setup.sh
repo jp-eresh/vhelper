742871e5b9ed64b9b81a5887a00d67770c4cb369
jp-eresh <48378674+jp-eresh@users.noreply.github.com>
2020-03-15 16:32:25 +0900

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo cp vhelper /usr/local/sbin/
-
-echo alias mkvenv=\"source vhelper make\" >> ~/.bashrc
-echo alias rmvenv=\"source vhelper remove\" >> ~/.bashrc
-echo alias workon=\"source vhelper workon\" >> ~/.bashrc
-
-source ~/.bashrc
